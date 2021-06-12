4c73107349e27b6da03f21d59f191c721e55b140
jabsatz <julianabsatz@gmail.com>
2021-06-12 17:23:32 +0200

    Added shoot points

--- Enemies/EnemyA.gd
+++ Enemies/EnemyA.gd
@@ -8,9 +8,13 @@ export (int) var speed = 40
 export (int) var damage = 40
 export (int) var vertical_speed = 15
 export (bool) var bounce_at_path_end = true
-var accumulated_offset = 0
+export (Array, int) var shoot_points = []
+var projectileBase = preload("res://Common/EnemyProjectile.tscn")
+var vertical_translation_offset = 0
+var projectile_speed = 400
+var projectile_damage = 10
 
-enum EnemyAStates { ENTER_STAGE, MOVING, DEAD }
+enum EnemyAStates { ENTER_STAGE, MOVING, SHOOTING, DEAD }
 const StateMachine = preload('res://Common/StateMachine.gd')
 
 onready var state_machine = StateMachine.new(EnemyAStates, self)
@@ -22,7 +26,6 @@ func _ready():
 	state_machine.set_state(EnemyAStates.ENTER_STAGE)
 	state_machine.set_side(initial_direction)
 	position = get_enter_position()
-	$Hitbox.connect('hit', self, '_on_hit')
 
 func get_enter_position():
 	if enter_direction == EnterOptions.TOP:
@@ -42,18 +45,45 @@ func _process_enter_stage(delta, _meta):
 
 func _process_moving(delta, _meta):
 	var side_multiplier = 1 if state_machine.current_side == StateMachine.Sides.FORWARDS else -1
-	var new_offset = get_offset() + speed * delta * side_multiplier
+	var old_offset = get_offset()
+	var new_offset = old_offset + speed * delta * side_multiplier
 	var total_offset = get_parent().curve.get_baked_length()
 	if bounce_at_path_end and (total_offset <= new_offset and side_multiplier == 1) or (new_offset <= 0 and side_multiplier == -1):
 		state_machine.flip_side()
+		if (shoot_points.has(0) and new_offset <= 0) or (shoot_points.has(get_parent().curve.get_point_count() - 1)and new_offset >= total_offset):
+			state_machine.set_state(EnemyAStates.SHOOTING)
 		return
 	
 	set_offset(new_offset)
-	accumulated_offset += vertical_speed * delta
-	global_translate(Vector2(0,accumulated_offset))
+	
+	for id in shoot_points:
+		var shoot_point = get_parent().curve.get_point_position(id)
+		if (shoot_point - position).length() < delta * speed / 2:
+			state_machine.set_state(EnemyAStates.SHOOTING)
+	
+	vertical_translation_offset += vertical_speed * delta
+	global_translate(Vector2(0,vertical_translation_offset))
 	# If we reach the end of the screen, start going back
 	if (global_position.y > 600 and vertical_speed > 0) or (global_position.y < 0 and vertical_speed < 0):
 		vertical_speed = -vertical_speed
+
+var shoot_timer = 0
+var shoot_time = 0.5
+func _on_shooting_start(_meta):
+	var player = get_node('/root/World/PlayerMove')
+	var direction = ( player.global_position - global_position).normalized()
+	var projectile = projectileBase.instance()
+	projectile.speed = projectile_speed
+	projectile.damage = projectile_damage
+	projectile.direction = direction
+	get_tree().get_root().add_child(projectile)
+	projectile.global_position = global_position
+	shoot_timer = 0
+
+func _process_shooting(delta, _meta):
+	shoot_timer += delta
+	if(shoot_timer >= shoot_time):
+		state_machine.set_state(EnemyAStates.MOVING)
 	
 func _physics_process(delta):
 	state_machine.process_step(delta)
