ee48388d34e6fa03da58ee6af3040fb593fbdb04
jabsatz <julianabsatz@gmail.com>
2021-06-13 19:53:55 +0200

    sonido canoa

--- Enemies/Canoa.gd
+++ Enemies/Canoa.gd
@@ -7,6 +7,8 @@ func all_state_process(_delta, _meta):
 	$SpriteGuy2.look_at(player.global_position)
 
 func _on_shooting_start(_meta):
+	if $disparo:
+		$disparo.play()
 	for guy in [$SpriteGuy1, $SpriteGuy2]:
 		var direction = get_target_shoot_direction_from(guy.global_position)
 		var projectile = projectileBase.instance()
